2e1e469dc2386c564dd424cb125bdb69d6d32aca
Ankur Satya <satya.ankur@gmail.com>
2023-02-25 23:43:55 +0100

    solution for 4.2 and 4.3 added

--- assignments/1/problem-4.R
+++ assignments/1/problem-4.R
@@ -1,37 +1,80 @@
 library(MASS)
 library(hash)
+library(dplyr)
+library(ggplot2)
 
-num_blocks = 6;
-num_plots = 4;
-
-data = hash()
-data["block"] = c();
-data["N"] = c();
-data["P"] = c();
-data["K"] = c();
-
-for (p in 1:num_blocks)
+# Problem 4.1
+randomise_additives <- function (num_blocks, num_plots)
 {
-  plots_for_n = sample(c(1:4), 2);
-  plots_for_p = sample(c(1:4), 2);
-  plots_for_k = sample(c(1:4), 2);
+  data = hash()
+  data["block"] = c();
+  data["N"] = c();
+  data["P"] = c();
+  data["K"] = c();
   
-  list_ = list(c(rep(0,4)), c(rep(0,4)), c(rep(0,4)));
-  
-  for (b in 1:2)
+  for (p in 1:num_blocks)
   {
-    list_[[1]][plots_for_n[b]] = 1;
-    list_[[2]][plots_for_p[b]] = 1;
-    list_[[3]][plots_for_k[b]] = 1;
-  }
+    plots_for_n = sample(c(1:4), 2);
+    plots_for_p = sample(c(1:4), 2);
+    plots_for_k = sample(c(1:4), 2);
     
-  data[["block"]] = c(data[["block"]], c(rep(p,4)));
-  data[["N"]] = c(data[["N"]], list_[[1]]);
-  data[["P"]] = c(data[["P"]], list_[[2]]);
-  data[["K"]] = c(data[["K"]], list_[[3]]);
+    list_ = list(c(rep(0,4)), c(rep(0,4)), c(rep(0,4)));
+    
+    for (b in 1:2)
+    {
+      list_[[1]][plots_for_n[b]] = 1;
+      list_[[2]][plots_for_p[b]] = 1;
+      list_[[3]][plots_for_k[b]] = 1;
+    }
+    
+    data[["block"]] = c(data[["block"]], c(rep(p,4)));
+    data[["N"]] = c(data[["N"]], list_[[1]]);
+    data[["P"]] = c(data[["P"]], list_[[2]]);
+    data[["K"]] = c(data[["K"]], list_[[3]]);
+  }
+  
+  dataframe = data.frame(Block = data[["block"]], N=data[["N"]], P=data[["P"]], K=data[["K"]])
+  return (dataframe)
 }
 
-dataframe = data.frame(Block = data[["block"]], N=data[["N"]], P=data[["P"]], K=data[["K"]])
-print(dataframe)
-write.csv(dataframe, "tmp.csv")
+
+# num_blocks = 6;
+# num_plots = 4;
+# 
+# rand_dataframe = randomise_additives(num_blocks, num_plots)
+# write.csv(rand_dataframe, "tmp.csv")
+
+# Problem 4.2
+
+plot_yield <- function(){
+  df = npk
+  nitrogen_present_yield = sum(filter(df, N==1)$yield)
+  nitrogen_absent_yield  = sum(filter(df, N==0)$yield)
+  
+  num_blocks = length(unique(npk$block))
+  
+  avg_yield_with_nitrogen = round(nitrogen_present_yield/num_blocks, 2)
+  avg_yield_wo_nitrogen = round(nitrogen_absent_yield/num_blocks, 2)
+  
+  jpeg("avg_yield.jpg")
+  barplot(c(avg_yield_with_nitrogen, avg_yield_wo_nitrogen), names.arg = c("With Nitrogen", "Without Nitrogen"), 
+          ylab="Average yield per block")
+}
+
+plot_yield()
+
+
+# Problem 4.3
+anova_block_and_nitrogen <- function(dataset){
+  dataset$block = as.factor(dataset$block)
+  dataset$N = as.factor(dataset$N)
+  
+  model = lm(yield~block*N, data=dataset)
+  return (anova(model))
+}
+
+anova_block_and_nitrogen(npk)
+
+
+
 
