84adddfa24bda00d46277e037c8a8066580ba731
Ankur Satya <satya.ankur@gmail.com>
2023-03-15 15:27:38 +0100

    added partial code for 3d

--- assignments/2/problem-3.R
+++ assignments/2/problem-3.R
@@ -14,29 +14,36 @@ graph_plot <- function (df)
   num_women_survived = nrow(filter(tmp_df, Sex=="female"))
   num_men_survived = nrow(filter(tmp_df, Sex=="male"))
   
+  total_women = nrow(filter(df, Sex=="female")) 
+  total_men = nrow(filter(df, Sex=="male")) 
+  
   jpeg("3a_survivors_based_on_sex.jpg")
-  barplot(c(round(100.0*num_women_survived/num_survivors, 2), 
-            round(100.0*num_men_survived/num_survivors, 2)), 
+  barplot(c(round(100.0*num_women_survived/total_women, 2), 
+            round(100.0*num_men_survived/total_men, 2)), 
           names.arg = c("Women", "Men"), 
-          ylab="Number survived", ylim=c(0, 100))
+          ylab="Percentage survived", ylim=c(0, 100))
   
   # Survival based on passenger class
-  num_first_class = nrow(filter(tmp_df, PClass=="1st"))
-  num_second_class = nrow(filter(tmp_df, PClass=="2nd"))
-  num_third_class = nrow(filter(tmp_df, PClass=="3rd"))
+  num_first_class_survived = nrow(filter(tmp_df, PClass=="1st"))
+  num_second_class_survived = nrow(filter(tmp_df, PClass=="2nd"))
+  num_third_class_survived = nrow(filter(tmp_df, PClass=="3rd"))
+  
+  num_first_class = nrow(filter(df, PClass=="1st"))
+  num_second_class = nrow(filter(df, PClass=="2nd"))
+  num_third_class = nrow(filter(df, PClass=="3rd"))
   
   jpeg("3a_survivors_based_on_class.jpg")
-  barplot(c(round(100.0*num_first_class/num_survivors, 2), 
-            round(100.0*num_second_class/num_survivors, 2), 
-            round(100.0*num_third_class/num_survivors, 2)), 
+  barplot(c(round(100.0*num_first_class_survived/num_first_class, 2), 
+            round(100.0*num_second_class_survived/num_second_class, 2), 
+            round(100.0*num_third_class_survived/num_third_class, 2)), 
           names.arg = c("First", "Second", "Third"), 
-          ylab="Number survived", ylim=c(0,100))
+          xlab="Passenger Class", ylab="Percentage survived", ylim=c(0,100))
   
   jpeg("3a_survivors_based_on_age.jpg")
-  hist_info <- hist(tmp_df$Age, , breaks=8, plot = FALSE)
+  hist_info <- hist(df$Age, , breaks=8, plot = FALSE)
   hist_info$density <- 100.0* hist_info$counts/sum(hist_info$counts)
-  plot(hist_info, freq = FALSE, ylim = c(0, 30), main="", 
-       xlab="Survivor Age", ylab="Percentage")
+  plot(hist_info, freq = FALSE, ylim = c(0, 40), main="", 
+       xlab="Survivor Age", ylab="Percentage survived")
   
   
 }
@@ -49,7 +56,7 @@ p3a_logit <- function(df)
   fit_model = glm(formula=Survived~Age+Sex_female+Sex_male+PClass_1st+PClass_2nd+PClass_3rd,
                   data=mod_df, family=binomial())
   
-  odds = or_glm(data=df, model=fit_model, incr=list(Age=5))
+  odds = or_glm(data=df, model=fit_model, incr=list(Age=10))
   print(anova(fit_model, test="Chisq"))
   print("_______________")
   print(odds)
@@ -93,12 +100,34 @@ p3b_final_model <- function(df)
   mod_test_data$probs = predictions
   print(mod_test_data)
 }
+
+p3d <- function(df)
+{
+  # Contingency table for PClass and Survived
+  pclass_con_table = table(df$PClass, df$Survived)
+  print(addmargins(pclass_con_table))
+  
+  # Contingency table for Sex and Survived
+  sex_con_table = table(df$Sex, df$Survived)
+  print(addmargins(sex_con_table))
+  
+  #Chi-Square test
+  print("Chi square test for PClass and Survived")
+  print(chisq.test(pclass_con_table))
+  
+  print("Chi Square test for Sex and Survived")
+  print(chisq.test(sex_con_table))
+  
+  
+}
   
 df = read.csv("/home/ankur/Ankur/CLS/Y2/P4/EDDA/assignments/2/titanic.txt",
               header=TRUE, sep="\t")
 
 # graph_plot(df)
-# p3a_logit(df)
+p3a_logit(df)
 # print("____________________")
 # p3b_testing_model(df)
-p3b_final_model(df)
+# p3b_final_model(df)
+# p3d(df)
+
